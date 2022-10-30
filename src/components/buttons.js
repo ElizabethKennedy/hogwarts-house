1a7dc83ba34be2f504e9757522953f8b552d071b
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2022-10-29 18:20:02 -0700

    amended buttons file

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,13 +1,6 @@
 class GameButtons {
     constructor(){
     this.buttonsContainer = document.getElementById('buttonsContainer')
-    this.triviaBtn = document.createElement('button')
-    this.triviaBtn.setAttribute('quiz_id', 1); 
-    this.triviaBtn.classList.add('btn')
-    this.triviaBtn.classList.add('btn-outline-danger')
-    this.triviaBtn.innerHTML = "Trivia Challenge"
-    this.triviaBtn.id = 'trivia'
-    this.triviaBtn.addEventListener("click", this.triviaButton)
     this.sortingBtn = document.createElement('button')
     this.sortingBtn.setAttribute('quiz_id', 2); 
     this.sortingBtn.classList.add('btn')
@@ -22,16 +15,6 @@ class GameButtons {
     this.buttonsContainer.appendChild(divider)
     this.explanation = document.getElementById('explanation')
   }
-
-  triviaButton = (event) => {
-    this.explanation.innerHTML = "This trivia game asks a series of questions about the Harry Potter franchise. Consider this your O.W.L.s. You will receive 5 points for your house if you answer 80% correct, and you will receive 10 points if you answer 100% correct."
-
-    let quizID = event.target.attributes.quiz_id.value
-    GameButtons.resetQuizSpace()
-    this.renderQuiz(quizID)
-    this.triviaBtn.innerHTML= "Restart Trivia"
-    this.resetSortingButton()
-  }
   
   sortingButton = (event) =>{
     this.explanation.innerHTML = "You will be asked a series of questions to determine if you are best suited for Hufflepuff, Gryffindor, Slytherin or Ravenclaw. If you already have a house and submit for sorting again, all points and scores you have earned for your old house will be deleted as you can no longer be counted in that house's point total."
@@ -43,19 +26,7 @@ class GameButtons {
     this.resetTriviaButton()
   }
 
-  static resetQuizSpace = () => {
-    let quizContainer = document.getElementById('contain')
-    let resultsContainer = document.getElementById('results')
-    quizContainer.innerHTML = ""
-    resultsContainer.innerHTML = ""
-    let divone = document.createElement('div')
-    divone.classList.add('quiz-container');
-    let divquiz = document.createElement('div')
-    divquiz.setAttribute('id', 'quiz')
-    divone.appendChild(divquiz)
-    quizContainer.appendChild(divone)
-  }
-
+  
   resetTriviaButton =() =>{
     this.triviaBtn.innerHTML = "Trivia Challenge"
    }
